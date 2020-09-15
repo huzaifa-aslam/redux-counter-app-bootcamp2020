c5cdefac7fe626c7ab91e3f9aca357e629726b0d
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-09-16 01:40:34 +0500

    added toolkit code

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -3,3 +3,15 @@ import {CounterReducer} from './CounterReducer';
 
 const store = createStore(CounterReducer)
 export default store
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import {CounterReducer} from './CounterReducer';
+
+// export default configureStore({
+//     reducer:{
+//         counter:CounterReducer
+//     }
+// })
+
+
---
+++ src/CounterSlice.js
@@ -0,0 +1,19 @@
+// import {createSlice} from '@reduxjs/toolkit'
+
+// export const counterSlice=createSlice({
+//     name:'counter',
+//     initialState:{
+//         value:0
+//     },
+//     reducer:{
+//         increament:state=>{
+//             state.value+=1
+//         },
+//         decreament:state=>{
+//             state.value-=1
+//         }
+//     }
+// })
+// export const {increament,decreament}=counterSlice.actions;
+// export const selectCount=(state)=>stete.counter.value
+// export default counterSlice.reducer
