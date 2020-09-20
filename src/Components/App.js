036ee0b737b1948776fe83ccd6f4f35cab8d589f
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-09-21 00:30:34 +0500

    ready for redux-toolkit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,14 @@
 import React,{useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {CounterInput} from './CounterInput'
-import {CounterOutput} from './CounterOutput'
+
 
 
 function App() {
-    const [counter,setCounter]=useState(0)
     return (
         <div className="App">
         <h1> Redux Practice </h1>
-        <CounterOutput counter={counter}/>
-        <CounterInput counter={counter} setCounter={setCounter}/>
+
 
          </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
 import "./index.css";
-import { store } from "./Components/Store";
-import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <Provider store={store}>
+
     <App />
-  </Provider>,
+  ,
   document.getElementById("root")
 );
